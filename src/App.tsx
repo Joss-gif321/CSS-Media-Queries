394cf6734242a7aea32d3e2f8003f9760884f496
Joss-gif321 <parakteno.321@gmail.com>
2026-09-04 12:50:04 +0000

    Responssividade de telas

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'; //
+export default function App() {
+  return (
+    <>
+      <nav className="navbar">
+      </nav>
+      <button className="botao-flutuante" type="button" aria-label="Adicionar">
+        +
+      </button>
+
+      <main>
+        <h1 className="mensagem"></h1>
+      </main>
+    </>
+  );
+}
